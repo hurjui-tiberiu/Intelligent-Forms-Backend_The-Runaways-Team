ed555292b7af095d10473983596e78fb2cd702a0
Hurjui Andrei-Tiberiu <94405226+hurjui-tiberiu@users.noreply.github.com>
2023-03-15 10:34:20 +0200

    Update SubmissionProfile.cs

--- IntelligentFormsAPI.Application/Mapper/SubmissionProfile.cs
+++ IntelligentFormsAPI.Application/Mapper/SubmissionProfile.cs
@@ -1,22 +1,25 @@
 using AutoMapper;
-using IntelligentFormsAPI.Application.Models;
-using IntelligentFormsAPI.Application.Models.User;
+using IntelligentFormsAPI.Application.Models.Submission;
 using IntelligentFormsAPI.Domain.Entities;
+using System.Text;
 
 namespace IntelligentFormsAPI.Application.Mapper
 {
-    public class UserProfile : Profile
+    public class SubmissionProfile : Profile
     {
-        public UserProfile()
+        public SubmissionProfile()
         {
-            CreateMap<UserSignUpDto, User>();
+            CreateMap<Submission, SubmissionRequestDto>()
+            .ForMember(dest => dest.Content,
+                opt => opt.MapFrom(src => Encoding.UTF8.GetString(Convert.FromBase64String(src.Content))))
+            .ForMember(dest => dest.TimeStamp,
+                opt => opt.MapFrom(src => src.TimeStamp.ToString("dd/MM/yy H:mm")));
 
-            CreateMap<User, UserDto>();
 
-            CreateMap<UserPatchDto, User>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
-
-            CreateMap<User, UserSignInResponseDto>();
+            CreateMap<SubmissionDto, Submission>()
+                .ForMember(dest => dest.Content,
+                opt => opt.MapFrom(
+                    src => Convert.ToBase64String(Encoding.UTF8.GetBytes(src.Content))));
 
         }
     }
